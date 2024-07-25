bc26011520247382241c3729436bf44bed18dd8c
Sean W Jasin <swjasin03@gmail.com>
2024-07-25 01:16:31 -0500

    Stopped segfaults and added streams

--- capture.c
+++ capture.c
@@ -26,7 +26,11 @@ int main(void) {
         printf("%s\n", uri);
     }
 
+
     struct iio_context *ctx = iio_create_context(NULL, uri);
+
+    iio_scan_destroy(scan);
+
     struct iio_device *dev;
     int dev_cnt = iio_context_get_devices_count(ctx);
     char *name;
@@ -43,17 +47,19 @@ int main(void) {
                iio_device_get_attrs_count(dev),
                iio_device_get_channels_count(dev));
         if(strcmp(name, "m2k-logic-analyzer-tx") == 0) {
-            free(name);
+            /* free(name); */
             break;
         }
-        free(name);
+        /* free(name); */
     }
+    /* free(dev); */
+
 
     int ch_cnt = iio_device_get_channels_count(dev);
     struct iio_channel *ch;
 
     struct iio_channels_mask *mask;
-    mask = iio_create_channels_mask(1);
+    mask = iio_create_channels_mask(16);
 
     for (int i = 0; i < ch_cnt; i++) {
         ch = iio_device_get_channel(dev, i);
@@ -62,21 +68,32 @@ int main(void) {
                name,
                iio_channel_is_scan_element(ch));
         iio_channel_enable(ch, mask);
-        free(name);
+        /* free(name); */
     }
+
 
     struct iio_buffer *buf;
     buf = iio_device_create_buffer(dev, 0, mask);
 
     struct iio_block *blk;
-    blk = iio_buffer_create_block(buf, 4096);
+    int size = iio_device_get_sample_size(dev, mask);
+    blk = iio_buffer_create_block(buf, size);
+
+    /* iio_block_destroy(blk); */
+    /* iio_buffer_destroy(buf); */
+    /* iio_channels_mask_destroy(mask); */
+    /* iio_context_destroy(ctx); */
+    /* return 0; */
 
     iio_buffer_enable(buf);
-    struct iio_stream *str = iio_buffer_create_stream(buf, 4, 64);
+    struct iio_stream *str = iio_buffer_create_stream(buf, 4, 2);
     blk = (struct iio_block *)iio_stream_get_next_block(str);
-    int size = iio_device_get_sample_size(dev, mask);
-    /* for (void *i = iio_block_start(blk); i < iio_block_end(blk); i += 10) { */
-    /*     printf("%b\n", i); */
+    /* void *start = iio_block_start(blk); */
+    /* int len = (intptr_t)iio_block_end(blk) - (intptr_t)start; */
+    /* for (; len; ) { */
+    /*     int nb = fwrite(start, 1, len, stdout); */
+    /*     len -= nb; */
+    /*     start = (void*)((intptr_t) start + nb); */
     /* } */
 
     iio_block_foreach_sample(blk, mask, sample_cb, NULL);
@@ -85,14 +102,29 @@ int main(void) {
     /*     ptr += iio_block_step(blk)) { */
     /* } */
     iio_buffer_disable(buf);
+
+    /* iio_stream_destroy(str); */
+    iio_block_destroy(blk);
+    iio_buffer_destroy(buf);
+    iio_channels_mask_destroy(mask);
+    iio_context_destroy(ctx);
+    /* NOTE: API does not allow for checking if iio_context has been properly
+     * shutdown/destroyed */
+    /* if(ctx) { */
+    /*     printf("\nCTX exists %d\n", iio_context_get_devices_count(ctx)); */
+    /*     return 0; */
+    /* } */
     return 0;
 }
 
-bool has_repeat;
+bool has_repeat = false;
 
 ssize_t sample_cb(const struct iio_channel *chn, void *src, size_t bytes, __notused void *d)
 {
+    /* void *dst; */
+    /* iio_channel_convert(chn, dst, src); */
 	const struct iio_data_format *fmt = iio_channel_get_data_format(chn);
+    /* printf("%s\n", (char *)dst); */
 	unsigned int j, repeat = has_repeat ? fmt->repeat : 1;
 
     /* iio_channel_convert(chn, d, src); */
@@ -100,9 +132,9 @@ ssize_t sample_cb(const struct iio_channel *chn, void *src, size_t bytes, __notu
 	printf("%s ", iio_channel_get_id(chn));
 	for (j = 0; j < repeat; ++j) {
 		if (bytes == sizeof(int16_t))
-			printf("%" PRIi16 " ", ((int16_t *)src)[j]);
+			printf("%" PRIi16 " \n", ((int16_t *)src)[j]);
 		else if (bytes == sizeof(int64_t))
-			printf("%" PRIi64 " ", ((int64_t *)src)[j]);
+			printf("%" PRIi64 " \n", ((int64_t *)src)[j]);
 	}
 
 	return bytes * repeat;
