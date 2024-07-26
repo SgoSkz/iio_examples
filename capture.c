SgoSkz/iio_examples

commit 987b6a1ecc3e93a0ea9876e9c66faebf1e01d3f7

No long have conflict enabling buffer device

capture.c
@@ -56,7 +56,6 @@ int main(void) {
     for (int i = 0; i < attr_cnt; i++) {
         attr = (struct iio_attr *)iio_device_get_attr(dev, i);
         (void)iio_attr_get_static_value(attr);
-        /* iio_attr_read_longlong(attr, data); */
         iio_attr_read_raw(attr, (char *)data, 8192*2-1);
         value = (char*)data;
         printf("\t\tAttr name: %s\n\
@@ -94,23 +93,68 @@ int main(void) {
         } else {
             iio_channel_disable(ch, mask);
         }
+
+    /* Read device attributes */
+    int attr_cnt = iio_device_get_attrs_count(dev);
+    struct iio_attr *attr;
+    void *data[8192*2];
+    char *value;
+    for (int i = 0; i < attr_cnt; i++) {
+        attr = (struct iio_attr *)iio_device_get_attr(dev, i);
+        (void)iio_attr_get_static_value(attr);
+        iio_attr_read_raw(attr, (char *)data, 8192*2-1);
+        value = (char*)data;
+        printf("\t\tAttr name: %s\n\
+\t\t\tAttr value (raw): %s\n",
+               iio_attr_get_name(attr),
+               value);
+    }
     }
 
 
     struct iio_buffer *buf;
     buf = iio_device_create_buffer(dev, 0, mask);
+    if(iio_err(buf)) {
+        printf("Failed to make buffer\n");
+    }
 
     struct iio_block *blk;
     int size = iio_device_get_sample_size(dev, mask);
     blk = iio_buffer_create_block(buf, size);
 
+    struct iio_stream *str;
+
     /* iio_buffer_enable is 0 on success */
-    if(iio_buffer_enable(buf) == 0) {
+    /* if(iio_buffer_enable(buf) == 0) { */
         printf("Buffer enabled!\n");
         /* NOTE: 4 blocks, 2 samples */
-        struct iio_stream *str = iio_buffer_create_stream(buf, 4, 16);
+        str = iio_buffer_create_stream(buf, 4, 1);
+        if (iio_err(str)) {
+            printf("Cannot make stream\n");
+            iio_buffer_disable(buf);
+
+            printf("Buffer disabled!\n");
+            iio_block_destroy(blk);
+            iio_buffer_destroy(buf);
+            iio_channels_mask_destroy(mask);
+            iio_context_destroy(ctx);
+
+            return 0;
+        }
 
         blk = (struct iio_block *)iio_stream_get_next_block(str);
+        if (iio_err(blk)) { 
+            printf("Cannot get next block\n");
+            iio_buffer_disable(buf);
+
+            printf("Buffer disabled!\n");
+            iio_block_destroy(blk);
+            iio_buffer_destroy(buf);
+            iio_channels_mask_destroy(mask);
+            iio_context_destroy(ctx);
+
+            return 0;
+        /* } */
 
         iio_block_foreach_sample(blk, mask, sample_cb, NULL);
 
@@ -136,7 +180,7 @@ int main(void) {
     return 0;
 }
 
-bool has_repeat = false;
+bool has_repeat = true;
 
 ssize_t sample_cb(const struct iio_channel *chn, void *src, size_t bytes, __notused void *d)
 {

commit 0c87e1273416ee9b037ad05caaee9ad7155d32c6

Fixed error introduced in 987b6a1

capture.c
@@ -88,27 +88,26 @@ int main(void) {
                iio_channel_is_scan_element(ch));
 
         /* If it can be enabled, enable it, otherwise disable (just in case) */
-        if(iio_channel_is_scan_element(ch)) {
+        if (iio_channel_is_scan_element(ch)) {
             iio_channel_enable(ch, mask);
         } else {
             iio_channel_disable(ch, mask);
         }
 
-    /* Read device attributes */
-    int attr_cnt = iio_device_get_attrs_count(dev);
-    struct iio_attr *attr;
-    void *data[8192*2];
-    char *value;
-    for (int i = 0; i < attr_cnt; i++) {
-        attr = (struct iio_attr *)iio_device_get_attr(dev, i);
-        (void)iio_attr_get_static_value(attr);
-        iio_attr_read_raw(attr, (char *)data, 8192*2-1);
-        value = (char*)data;
-        printf("\t\tAttr name: %s\n\
+        /* Read device attributes */
+        int attr_cnt = iio_device_get_attrs_count(dev);
+        struct iio_attr *attr;
+        void *data[8192 * 2];
+        char *value;
+        for (int i = 0; i < attr_cnt; i++) {
+            attr = (struct iio_attr *)iio_device_get_attr(dev, i);
+            (void)iio_attr_get_static_value(attr);
+            iio_attr_read_raw(attr, (char *)data, 8192 * 2 - 1);
+            value = (char *)data;
+            printf("\t\tAttr name: %s\n\
 \t\t\tAttr value (raw): %s\n",
-               iio_attr_get_name(attr),
-               value);
-    }
+                   iio_attr_get_name(attr), value);
+        }
     }
 
 
@@ -116,6 +115,17 @@ int main(void) {
     buf = iio_device_create_buffer(dev, 0, mask);
     if(iio_err(buf)) {
         printf("Failed to make buffer\n");
+    }
+    int buf_attrs_cnt = iio_buffer_get_attrs_count(buf);
+    {
+        void *data[8192 * 2];
+        char *value;
+        for (int i = 0; i < buf_attrs_cnt; i++) {
+            struct iio_attr *attr = (struct iio_attr *)iio_buffer_get_attr(buf, i);
+            iio_attr_read_raw(attr, (char *)data, 8192 * 2 - 1);
+            value = (char *)data;
+            printf("Attr name: %s\n\tValue: %s\n", iio_attr_get_name(attr), value);
+        }
     }
 
     struct iio_block *blk;
@@ -128,7 +138,7 @@ int main(void) {
     /* if(iio_buffer_enable(buf) == 0) { */
         printf("Buffer enabled!\n");
         /* NOTE: 4 blocks, 2 samples */
-        str = iio_buffer_create_stream(buf, 4, 1);
+        str = iio_buffer_create_stream(buf, 4, 2560);
         if (iio_err(str)) {
             printf("Cannot make stream\n");
             iio_buffer_disable(buf);
@@ -154,13 +164,13 @@ int main(void) {
             iio_context_destroy(ctx);
 
             return 0;
-        /* } */
+        }
 
         iio_block_foreach_sample(blk, mask, sample_cb, NULL);
 
         iio_buffer_disable(buf);
         printf("Buffer disabled!\n");
-    }
+    /* } */
 
     /* iio_stream_destroy(str); */
     iio_block_destroy(blk);